21e1824a4d7dd4379454548f05c93e75be6bee1b
Aniket-ML2AI <aniketphadnis6@gmail.com>
2024-11-09 14:47:24 +0530

    Fixed the function names

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,11 +1,12 @@
-from src.maths_operations import _add,_sub
+from src.maths_operations import add,sub
 
 def test_add():
-    assert _add(2,3)==5
-    assert _add (-1,1) ==0
-    assert _add(4,4) == 8
+    assert add(2,3)==5
+    assert add (-1,1) ==0
+    assert add(4,4) == 8
 
 def test_sub():
-    assert _sub(1,1) ==0
-    assert _sub (2,1) == 1
+    assert sub(1,1) ==0
+    assert sub (2,1) == 1
+
 
